e6cb2b342776ae481da35d9856b612e579a0928b
mikepepping <mikepepping@gmail.com>
2024-08-10 15:48:00 +1000

    Refactored test to be a while loop that counts from 10 to 0

--- test.rb
+++ test.rb
@@ -1,14 +1,3 @@
 require_relative 'interpreter'
 
-code = <<-CODE
-  PUSH_LIT 1
-  STORE_VAL one
-  PUSH_LIT 2
-  STORE_VAL two
-  PUSH_VAL one
-  PUSH_VAL two
-  ADD
-  PRINT
-CODE
-
-Interpreter.new.run(code)
+Interpreter.new(open('test.aphid')).run
